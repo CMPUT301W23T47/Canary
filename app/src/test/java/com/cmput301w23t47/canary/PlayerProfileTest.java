55ce39d93b14f54248a1a1eab2a41df2952a42db
Ajharris1344 <andrewharris1344@gmail.com>
2023-03-13 09:28:41 -0600

    Update PlayerProfileTest.java

--- app/src/test/java/com/cmput301w23t47/canary/PlayerProfileTest.java
+++ app/src/test/java/com/cmput301w23t47/canary/PlayerProfileTest.java
@@ -1,5 +1,10 @@
 package com.cmput301w23t47.canary;
 
+import androidx.fragment.app.Fragment;
+
+import com.cmput301w23t47.canary.model.Player;
+import com.cmput301w23t47.canary.view.fragment.PlayerProfileFragment;
+
 import org.junit.Test;
 
 public class PlayerProfileTest {
@@ -7,6 +12,21 @@ public class PlayerProfileTest {
 	@Test
 	public void testPlayerProfile() {
 	
-		// write code that
+		// Write a test to verify that the player has a profile page that displays their username, score, etc
+		// Write a test to verify that the player can edit their profile page
+		
+		String username = "officialTester";
+		String firstName = "testy";
+		String lastName = "tester";
+		int score = 100;
+		
+		Player player = new Player(username, firstName, lastName);
+		
+		Fragment fragment = new PlayerProfileFragment().newInstance();
+		
+		fragment
+		
+		
+		
 	}
 }
