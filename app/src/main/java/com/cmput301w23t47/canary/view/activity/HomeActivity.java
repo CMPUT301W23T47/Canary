55aba06ee04317c508fb8169502e01cbe520bc0d
Meharpreet Singh Nanda <msnanda515@gmail.com>
2023-03-13 11:40:42 -0600

    Add a different activity for home

--- app/src/main/java/com/cmput301w23t47/canary/view/activity/HomeActivity.java
+++ app/src/main/java/com/cmput301w23t47/canary/view/activity/HomeActivity.java
@@ -1,37 +1,31 @@
-package com.cmput301w23t47.canary;
+package com.cmput301w23t47.canary.view.activity;
 
+import android.os.Bundle;
+
+import com.cmput301w23t47.canary.databinding.ActivityHomeBinding;
 import androidx.appcompat.app.AppCompatActivity;
+
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import android.os.Bundle;
+import com.cmput301w23t47.canary.R;
 
-import com.cmput301w23t47.canary.databinding.ActivityMainBinding;
+public class HomeActivity extends AppCompatActivity {
 
-
-/**
- * Main Acitvity
- * @author Meharpreet Singh Nanda
- */
-public class MainActivity extends AppCompatActivity {
-    private ActivityMainBinding binding;
     private AppBarConfiguration appBarConfiguration;
+    private ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        init(savedInstanceState);
-    }
+        setSupportActionBar(binding.toolbar);
 
-    /**
-     * Initialization for activity
-     */
-    private void init(Bundle savedInstanceState) {
         initNavbar();
     }
 
